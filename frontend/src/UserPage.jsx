68d13abdaf3b5bb6e1cb41b9415c064678c4c2c5
sockl <sockl@hotmail.com>
2025-09-03 10:09:26 +0800

    Added Userpage component

---
+++ frontend/src/UserPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const UserPage = () => {
+  return (
+    <div>
+        <h2>User managemnt</h2>
+        <p>Here we will list all the users and manage users.</p>
+    </div>
+  )
+}
+export default UserPage
